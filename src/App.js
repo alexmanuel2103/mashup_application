c730d87b468805a8d562a41695dd791e232b0ac4
Salvador Perez <a6520150017@utch.edu.mx>
2022-03-29 20:07:18 -0700

    Request to Marvel API and changes in the background and some styles in the images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import  axios from 'axios';
+
+
+//https://gateway.marvel.com:443/v1/public/characters?apikey=84c9afc670217a09f2566ca3189177f9
+
+//key publica: 84c9afc670217a09f2566ca3189177f9
+//key privada: 9a4f1874e8f4c201b8895f1aaf372fdf3fa614a1
+//ts: 1
+
+//19a4f1874e8f4c201b8895f1aaf372fdf3fa614a184c9afc670217a09f2566ca3189177f9
+
+//hash: 273ddb932eb9647e18e21c589d55f000
+
+//example url :https://gateway.marvel.com:443/v1/public/characters?series=19&ts=1&apikey=84c9afc670217a09f2566ca3189177f9&hash=273ddb932eb9647e18e21c589d55f000
+
+
 
 function App() {
+
+  //esta es una variable para guardar los personajes
+  const [characters, setCharacters]=useState([])
+
+
+  useEffect(()=>{
+    //Usando axios obtenemos la informacion del link de marvel
+    axios.get('https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&title=ghost%20rider&ts=1&apikey=84c9afc670217a09f2566ca3189177f9&hash=273ddb932eb9647e18e21c589d55f000').then(res=>{
+        setCharacters(res.data.data.results)
+        
+    }).catch(error=>console.log(error))
+    
+
+  },[])
+
+  console.log(characters)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I LOVE YOU
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Python
-        </a>
-      </header>
+    <React.Fragment>
+      <div className='App'>
+      <div  className='App_Header'>
+      </div>
+        <div className='Characters_data'>
+          { characters.map( per=>(
+
+            <div className="Character__container">
+                <img
+                    className="Character__image" alt=''
+                    src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                />
+            <div className="Character__data">
+                <h1 className="Character__name">Name: {per.title}</h1>
+                <h2 className="Character__name">Stories: {per.stories.available}</h2>
+                <h3 className="Character__name">Series: {per.series.name}</h3>
+                <h3 className="Character__name">Format: {per.format}</h3>
+            </div>
+        </div>
+
+          ))}
+  
+
+        </div>
     </div>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App; 
